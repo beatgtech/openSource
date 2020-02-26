eec4fe0a9010a24fbf835698ae01ed3ee6a07a47
beatgtech <beatgtech@139.com>
2020-02-26 20:05:00 +0800

    update junit

--- practice/src/test/java/com/sen/chars/IntToCharTest.java
+++ practice/src/test/java/com/sen/chars/IntToCharTest.java
@@ -15,10 +15,10 @@ class IntToCharTest {
 
     @Test
     void testStage2method() {
-        Assert.assertEquals("att atu atv aut auu auv avt avu avv btt btu btv but buu buv bvt bvu bvv ctt ctu ctv cut cuu cuv cvt cvu cvv", IntToChar.stage1method(new int[]{2, 88}));
-        Assert.assertNull(IntToChar.stage1method(new int[]{0, 1}));
-        Assert.assertNull(IntToChar.stage1method(new int[]{8, 100}));
-        Assert.assertNull(IntToChar.stage1method(new int[]{-1, 1}));
+        Assert.assertEquals("att atu atv aut auu auv avt avu avv btt btu btv but buu buv bvt bvu bvv ctt ctu ctv cut cuu cuv cvt cvu cvv", IntToChar.stage2method(new int[]{2, 88}));
+        Assert.assertNull(IntToChar.stage2method(new int[]{0, 1}));
+        Assert.assertNull(IntToChar.stage2method(new int[]{8, 100}));
+        Assert.assertNull(IntToChar.stage2method(new int[]{-1, 1}));
     }
 //
 //    @Test
